1fae97236b21be2ed00b0d2abefdaa3317589ab4
tunisha43 <royalkaribo@gmail.com>
2026-08-19 11:24:52 +0100

    Update QtyStepper.tsx

--- made-in-nigeria-next-1/made-in-nigeria-next/components/product/QtyStepper.tsx
+++ made-in-nigeria-next-1/made-in-nigeria-next/components/product/QtyStepper.tsx
@@ -1,24 +1,25 @@
 'use client';
 
-import { useState } from 'react';
+interface QtyStepperProps {
+  value: number;
+  onChange: (qty: number) => void;
+}
 
-export default function QtyStepper() {
-  const [qty, setQty] = useState(1);
-
+export default function QtyStepper({ value, onChange }: QtyStepperProps) {
   return (
     <div className="qty-stepper">
       <button
         type="button"
         aria-label="Decrease quantity"
-        onClick={() => setQty((q) => Math.max(1, q - 1))}
+        onClick={() => onChange(Math.max(1, value - 1))}
       >
         &minus;
       </button>
-      <span>{qty}</span>
+      <span>{value}</span>
       <button
         type="button"
         aria-label="Increase quantity"
-        onClick={() => setQty((q) => q + 1)}
+        onClick={() => onChange(value + 1)}
       >
         +
       </button>
